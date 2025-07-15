943e4f9af1892674262b4c8b9fcb07462bc00306
Jan T. Sott <git@idleberg.com>
2025-07-16 00:04:22 +0200

    feat: add support for basic options

--- src/cli.ts
+++ src/cli.ts
@@ -40,7 +40,22 @@ export class CosmicEsbuild {
 			.option('-c, --config <file>', 'Path to the configuration file')
 			.option('-w, --watch', 'Run in watch mode', false)
 			.option('--clean', 'Clean output directory before building', false)
-			.option('--debug', 'Enable debug output', false);
+			.option('--debug', 'Enable debug output', false)
+
+			// ESbuild simple options
+			.optionsGroup('ESBuild: Simple Options')
+			.option('--bundle', 'Bundle all dependencies into the output files')
+			.option('--define <K=V...>', 'Substitute K with V while parsing')
+			.option('--external <module...>', 'Exclude module M from the bundle')
+			.option('--format <iife|cjs|esm>', 'Output format')
+			.option('--loader <X=L>', 'Use loader L to load file extension X')
+			.option('--minify', 'Minify the output')
+			.option('--outdir', 'The output directory (for multiple entry points)')
+			.option('--outfile', 'The output file (for one entry point)')
+			.option('--packages <bundle|external>', 'Set to "external" to avoid bundling any package')
+			.option('--sourcemap', 'Emit a source map')
+			.option('--splitting', 'Enable code splitting')
+			.option('--target <target...>', 'Environment target');
 
 		program.parse();
 		const options = program.opts();
@@ -72,10 +87,61 @@ export class CosmicEsbuild {
 				return defaultBuildOptions;
 			}
 
-			return {
+			const options = {
 				...defaultBuildOptions,
 				...result.config,
 			};
+
+			if (typeof this.options.bundle !== 'undefined') {
+				options.bundle = this.options.bundle;
+			}
+
+			if (this.options.define) {
+				options.define = Object.fromEntries(this.options.define.map((item: string) => item.split('=')));
+			}
+
+			if (this.options.external) {
+				options.external = this.options.external;
+			}
+
+			// TODO refine types
+			if (this.options.format) {
+				options.external = this.options.format;
+			}
+
+			if (this.options.loader) {
+				options.loader = Object.fromEntries(this.options.define.map((item: string) => item.split('=')));
+			}
+
+			if (typeof this.options.minify !== 'undefined') {
+				options.minify = this.options.minify;
+			}
+
+			if (typeof this.options.outdir !== 'undefined') {
+				options.outdir = this.options.outdir;
+			}
+
+			if (typeof this.options.outfile !== 'undefined') {
+				options.outfile = this.options.outfile;
+			}
+
+			if (this.options.packages) {
+				options.packages = this.options.packages;
+			}
+
+			if (typeof this.options.sourcemap !== 'undefined') {
+				options.sourcemap = this.options.sourcemap;
+			}
+
+			if (typeof this.options.splitting !== 'undefined') {
+				options.splitting = this.options.splitting;
+			}
+
+			if (this.options.target) {
+				options.target = this.options.target;
+			}
+
+			return options;
 		} catch (error) {
 			// Found configuration file, but an error occurred while parsing it.
 			this.logger.error('Error loading configuration:', (error as Error).message);
