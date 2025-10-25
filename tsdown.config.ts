5725ffd5211d6eabc6a151578d015bd250021f3f
Jan T. Sott <git@idleberg.com>
2025-10-25 12:19:05 +0200

    chore: update tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,26 +1,25 @@
-import { defineConfig } from 'tsdown';
+import { defineConfig, type UserConfig } from 'tsdown';
 
 export default defineConfig((options) => {
 	const isProduction = options.watch !== true;
 
 	const sharedConfig = {
-		clean: isProduction,
 		dts: isProduction,
-		format: ['esm', 'cjs'],
 		minify: isProduction,
-		platform: 'node',
 		target: 'node18',
-	};
+	} satisfies UserConfig;
 
 	return [
 		{
 			...sharedConfig,
 			entry: 'src/index.ts',
+			format: 'esm',
 			outDir: 'bin',
 		},
 		{
 			...sharedConfig,
 			entry: 'src/config.ts',
+			format: ['esm', 'cjs'],
 			outDir: 'lib',
 		},
 	];
