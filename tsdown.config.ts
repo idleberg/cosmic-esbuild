47b2f0d3c0fcde4df5064e694421d7e5e6d1038b
Jan T. Sott <git@idleberg.com>
2025-10-26 16:29:22 +0100

    fix: prevent rebuild loop

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,25 +3,16 @@ import { defineConfig, type UserConfig } from 'tsdown';
 export default defineConfig((options) => {
 	const isProduction = options.watch !== true;
 
-	const sharedConfig = {
+	return {
 		clean: isProduction,
 		dts: isProduction,
+		entry: {
+			cli: 'src/index.ts',
+			config: 'src/config.ts',
+		},
+		format: ['esm', 'cjs'],
 		minify: isProduction,
+		outDir: 'lib',
 		target: 'node18',
-	} satisfies UserConfig;
-
-	return [
-		{
-			...sharedConfig,
-			entry: 'src/index.ts',
-			format: 'esm',
-			outDir: 'bin',
-		},
-		{
-			...sharedConfig,
-			entry: 'src/config.ts',
-			format: ['esm', 'cjs'],
-			outDir: 'lib',
-		},
-	];
+	};
 });
